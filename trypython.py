ba80f4b88a76c833e6ac766df96b2a6bea16ea10
Stephen Afolabi <stephenafolabi@gmail.com>
2018-05-14 12:10:44 +0000

    initial on pythom

---
+++ trypython.py
@@ -0,0 +1,19 @@
+# print('Hello World')
+# print('Welcome To Python')
+# response = input("what's your name? ")
+# print('Hello' + response)
+
+# 
+# print('please enter two numbers')
+# a = int(input('enter number 1:'))
+# b = int(input('enter number 2:'))
+# print(a + b)
+
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+total = num1 + num2
+# print (str(num1))
+
+print(result = str(num1) + " plus " + str(num2) + " = " + str(total))
+
+
