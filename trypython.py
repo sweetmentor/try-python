001a2a56f0d6754798de920e332046714eea140f
Stephen Afolabi <stephenafolabi@gmail.com>
2018-05-15 10:23:37 +0000

    python challenge

--- trypython.py
+++ trypython.py
@@ -9,11 +9,58 @@
 # b = int(input('enter number 2:'))
 # print(a + b)
 
-num1 = int(input("Enter First Number: "))
-num2 = int(input("Enter Second Number: "))
-total = num1 + num2
+# num1 = int(input("Enter First Number: "))
+# num2 = int(input("Enter Second Number: "))
+# total = num1 + num2
 # print (str(num1))
 
-print(result = str(num1) + " plus " + str(num2) + " = " + str(total))
+#print(str(num1) + " plus " + str(num2) + " = " + str(total))
 
+# print("Before the function")
 
+# def add(x, y):
+#     print(x)
+#     print(y)
+#     return x + y
+
+# print("After the function")
+
+# print("Before calling the function")
+    
+# r = add(5, 4)
+
+# print("After calling the function")
+
+# print(r)
+
+# print("After printing the result")
+
+# text = input("what is your name:")
+# text = text.upper()
+
+# print(text)
+
+#-------------------challenge47----
+# num = int(input("please enter your number"))
+# if num <= 10:
+#     print ("Small")
+# else:
+#     print ("Big")
+#-------------------challenge48----
+# num1 = int(input("enter num1:"))
+# num2 = int(input("enter num2:"))
+# if num1 == num2:
+#     print ("Same")
+# else:
+#     print ("Different")
+#-------------challenge49-------
+# num = input("enter a number:")
+# if num == "1":
+#     name = input("please enter your name")
+#     print ("Your name is %s" % name)
+# if num == "2":
+#     age = input("enter your age: ")
+    
+#     age_int = int(age)
+#     print ("Your age is " + age)
+    
